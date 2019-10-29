513b1e22c70a0a803ee9b4536525fa87c0a59d01
TaboDoge <48008376+TaboDoge@users.noreply.github.com>
2019-10-29 16:13:07 -0700

    Update StringMethods.java

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -11,28 +11,23 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-String Thing = "aaaaaaaaa7aaaaaaaaaaaaaaaaaa";
+		String Thing = "aaaaaaaaa7aaaaaaaaaaaaaaaaaa";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		System.out.println(Thing.charAt(6));
+		System.out.println(Thing.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
 		System.out.println(Thing.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-		int r = -1;
-			for(int i =0; i < 28; i++) {
-				r++;
-				r = r * 4;
-			 if(Thing.charAt(r)==7) {
-				System.out.println("7 is at index "+ r)	;
-				}
-			 else {
-				 
-			 }
+		for(int i =0; i < Thing.length(); i++) {
+		 	System.out.println("charAt("+i+")="+Thing.charAt(i));		
+		 	if(Thing.charAt(i)=='7') {
+				System.out.println("7 is at index "+ i)	;
+			}
 				
-			}
-			}
+		}
+	}
 		
 			
 		//BROKEN.
